d2646a0f09f183e94f503c97af79d7d51f08dec5
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-10 15:06:10 -0600

    Fixing formatting

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "helm_release" "traefik" {
   version          = var.traefik_release_version
   namespace        = var.traefik_release_namespace
   create_namespace = var.traefik_create_namespace
-  values           = [
+  values = [
     templatefile("${path.module}/values.yml", {
       is_default_class = var.traefik_ingress_is_default_class
       entrypoints      = var.traefik_enabled_entrypoints
